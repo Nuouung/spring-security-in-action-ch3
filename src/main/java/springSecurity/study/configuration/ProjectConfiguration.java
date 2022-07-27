a0929bb2f6d7bfdc0602f55e87ceced3072ec2a1
Nuouung <jin6016@gmail.com>
2022-07-27 19:35:43 +0900

    jdbcUserDetailsManager 커스텀하기 (데이터베이스 테이블 및 컬럼 이름 설정 가능)

--- src/main/java/springSecurity/study/configuration/ProjectConfiguration.java
+++ src/main/java/springSecurity/study/configuration/ProjectConfiguration.java
@@ -6,6 +6,7 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 
 import javax.sql.DataSource;
 
@@ -14,7 +15,17 @@ public class ProjectConfiguration {
 
     @Bean
     public UserDetailsService userDetailsService(DataSource dataSource) {
-        return new JdbcUserDetailsManager(dataSource);
+        String usersByUsernameQuery = "select username_haha, password_haha, enabled_haha " +
+                "from users where username_haha = ?";
+
+        String authsByUserQuery = "select username_haha, authority_haha " +
+                "from authorities where username_haha = ?";
+
+        JdbcUserDetailsManager userDetailsManager = new JdbcUserDetailsManager(dataSource);
+        userDetailsManager.setUsersByUsernameQuery(usersByUsernameQuery);
+        userDetailsManager.setAuthoritiesByUsernameQuery(authsByUserQuery);
+
+        return userDetailsManager;
     }
 
     @Bean
