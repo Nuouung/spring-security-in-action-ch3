1ae47d06263486a40b57898efa828dc3a5cd6302
Nuouung <jin6016@gmail.com>
2022-08-06 14:09:22 +0900

    PlainTextPasswordEncoder, Sha512PasswordEncoder 구현과 DelegatingPasswordEncoder 구현 (PasswordEncoder - 암호화)

--- src/main/java/springSecurity/study/configuration/ProjectConfiguration.java
+++ src/main/java/springSecurity/study/configuration/ProjectConfiguration.java
@@ -3,33 +3,35 @@ package springSecurity.study.configuration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.scrypt.SCryptPasswordEncoder;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.provisioning.UserDetailsManager;
+import springSecurity.study.security.user.CustomUser;
 
 import javax.sql.DataSource;
+import java.util.HashMap;
 
 @Configuration
 public class ProjectConfiguration {
 
     @Bean
     public UserDetailsService userDetailsService(DataSource dataSource) {
-        String usersByUsernameQuery = "select username_haha, password_haha, enabled_haha " +
-                "from users where username_haha = ?";
-
-        String authsByUserQuery = "select username_haha, authority_haha " +
-                "from authorities where username_haha = ?";
-
-        JdbcUserDetailsManager userDetailsManager = new JdbcUserDetailsManager(dataSource);
-        userDetailsManager.setUsersByUsernameQuery(usersByUsernameQuery);
-        userDetailsManager.setAuthoritiesByUsernameQuery(authsByUserQuery);
-
-        return userDetailsManager;
+        return new JdbcUserDetailsManager(dataSource);
     }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return NoOpPasswordEncoder.getInstance();
+
+        HashMap<String, PasswordEncoder> encoders = new HashMap<>();
+
+        encoders.put("noop", NoOpPasswordEncoder.getInstance());
+        encoders.put("bcrypt", new BCryptPasswordEncoder());
+        encoders.put("scrypt", new SCryptPasswordEncoder());
+
+        return new DelegatingPasswordEncoder("bcrypt", encoders);
     }
 }
