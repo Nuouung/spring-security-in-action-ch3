a63210aab09fb807b9ba713aaf05b5967d1df948
Nuouung <jin6016@gmail.com>
2022-08-11 20:17:49 +0900

    http formLogin과 인증 성공, 실패 시 사용자가 밟게 될 로직 추가

--- src/main/java/springSecurity/study/configuration/ProjectConfiguration.java
+++ src/main/java/springSecurity/study/configuration/ProjectConfiguration.java
@@ -1,52 +1,7 @@
 package springSecurity.study.configuration;
 
-import lombok.RequiredArgsConstructor;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.crypto.scrypt.SCryptPasswordEncoder;
-import org.springframework.security.provisioning.JdbcUserDetailsManager;
-import org.springframework.security.provisioning.UserDetailsManager;
-import org.springframework.security.web.SecurityFilterChain;
-import springSecurity.study.security.user.CustomUser;
-
-import javax.sql.DataSource;
-import java.util.HashMap;
 
 @Configuration
-//@RequiredArgsConstructor
 public class ProjectConfiguration {
-
-//    private final AuthenticationProvider authenticationProvider;
-
-    @Bean
-    public UserDetailsService userDetailsService(DataSource dataSource) {
-        return new JdbcUserDetailsManager(dataSource);
-    }
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-
-        HashMap<String, PasswordEncoder> encoders = new HashMap<>();
-
-        encoders.put("noop", NoOpPasswordEncoder.getInstance());
-        encoders.put("bcrypt", new BCryptPasswordEncoder());
-        encoders.put("scrypt", new SCryptPasswordEncoder());
-
-        return new DelegatingPasswordEncoder("bcrypt", encoders);
-    }
-
-//    @Bean
-//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-//        http.authenticationProvider(authenticationProvider);
-//
-//        return http.build();
-//    }
 }
