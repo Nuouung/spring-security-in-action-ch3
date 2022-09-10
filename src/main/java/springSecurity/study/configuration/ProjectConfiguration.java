7b1718f95edd73a97e74531ce3253b9da24042f7
Nuouung <jin6016@gmail.com>
2022-09-10 18:38:31 +0900

    인가 mvc matcher

--- src/main/java/springSecurity/study/configuration/ProjectConfiguration.java
+++ src/main/java/springSecurity/study/configuration/ProjectConfiguration.java
@@ -3,14 +3,32 @@ package springSecurity.study.configuration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-public class ProjectConfiguration extends WebSecurityConfigurerAdapter {
+@EnableWebSecurity
+public class ProjectConfiguration {
+
+//    @Bean
+//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+//        http.httpBasic();
+//
+//        http.authorizeRequests()
+//                .mvcMatchers("/hello").hasRole("ADMIN") // csrf 방어 정책 때문에 GET만 들여보내고 나머지는 전부 Forbidden 처리한다.
+//                .mvcMatchers("/ciao").hasRole("MANAGER")
+////                .anyRequest().permitAll()
+////                .anyRequest().denyAll();
+//                .anyRequest().permitAll(); // 선언 순서는 구체적인 것을 위로 추상적인 것을 아래로
+//
+//
+//        return http.build();
+//    }
 
     @Bean
     public UserDetailsService userDetailsService() {
@@ -20,13 +38,5 @@ public class ProjectConfiguration extends WebSecurityConfigurerAdapter {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return NoOpPasswordEncoder.getInstance();
-    }
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.httpBasic();
-
-        http.authorizeRequests()
-                .anyRequest().hasRole("ADMIN"); // 접두사 ROLE_ 은 역할을 선언할 때만 사용. 역할을 사용할 때는 빼고 쓴다.
     }
 }
