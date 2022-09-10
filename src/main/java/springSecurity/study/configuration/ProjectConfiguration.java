2532ce98b1d67558cc5e4ec701541353a4feaf09
Nuouung <jin6016@gmail.com>
2022-09-10 18:57:00 +0900

    인가 mvc matcher 추가

--- src/main/java/springSecurity/study/configuration/ProjectConfiguration.java
+++ src/main/java/springSecurity/study/configuration/ProjectConfiguration.java
@@ -2,6 +2,7 @@ package springSecurity.study.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -15,20 +16,20 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 public class ProjectConfiguration {
 
-//    @Bean
-//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-//        http.httpBasic();
-//
-//        http.authorizeRequests()
-//                .mvcMatchers("/hello").hasRole("ADMIN") // csrf 방어 정책 때문에 GET만 들여보내고 나머지는 전부 Forbidden 처리한다.
-//                .mvcMatchers("/ciao").hasRole("MANAGER")
-////                .anyRequest().permitAll()
-////                .anyRequest().denyAll();
-//                .anyRequest().permitAll(); // 선언 순서는 구체적인 것을 위로 추상적인 것을 아래로
-//
-//
-//        return http.build();
-//    }
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.httpBasic();
+
+        http.authorizeRequests()
+//                .mvcMatchers(HttpMethod.GET, "/a/**").authenticated()
+                .mvcMatchers(HttpMethod.GET, "/a/{more:^[0-9]*$}").authenticated() // 정규식도 사용 가능
+                .mvcMatchers(HttpMethod.POST, "/a/**").permitAll()
+                .anyRequest().denyAll();
+
+        http.csrf().disable();
+
+        return http.build();
+    }
 
     @Bean
     public UserDetailsService userDetailsService() {
--- src/main/java/springSecurity/study/controller/HelloController.java
+++ src/main/java/springSecurity/study/controller/HelloController.java
@@ -1,23 +1,35 @@
 package springSecurity.study.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
-    @GetMapping("/hello")
-    public String hello() {
-        return "Hello!!";
+    @GetMapping("/a")
+    public String getEndpointA() {
+        return "Works!";
     }
 
-    @GetMapping("/ciao")
-    public String ciao() {
-        return "Ciao!!";
+    @GetMapping("/a/{more}")
+    public String getEndpointA(@PathVariable String more) {
+        return more;
     }
 
-    @GetMapping("/hola")
-    public String hola() {
-        return "Hola!!";
+    @PostMapping("/a")
+    public String postEndpointA() {
+        return "Works!";
+    }
+
+    @GetMapping("/b")
+    public String getEndpointB() {
+        return "Works!";
+    }
+
+    @PostMapping("/b")
+    public String postEndpointB() {
+        return "Works!";
     }
 }
