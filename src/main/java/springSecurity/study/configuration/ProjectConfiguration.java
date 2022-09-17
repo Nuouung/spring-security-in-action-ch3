baef8c2cc8dcbed7109d93b64cb9e8083425c74c
Nuouung <jin6016@gmail.com>
2022-09-17 16:21:31 +0900

    시큐리티 필터 체인에 속한 특정 필터 위치에 커스텀 필터 추가하기

--- src/main/java/springSecurity/study/configuration/ProjectConfiguration.java
+++ src/main/java/springSecurity/study/configuration/ProjectConfiguration.java
@@ -1,5 +1,7 @@
 package springSecurity.study.configuration;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -8,23 +10,23 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import springSecurity.study.security.filter.AuthenticationLoggingFilter;
 import springSecurity.study.security.filter.RequestValidationFilter;
+import springSecurity.study.security.filter.StaticKeyAuthenticationFilter;
+
+import javax.servlet.Filter;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class ProjectConfiguration {
+
+    private final StaticKeyAuthenticationFilter staticKeyAuthenticationFilter;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
-        http.addFilterBefore(
-                        new RequestValidationFilter(),
-                        BasicAuthenticationFilter.class)
-                .addFilterAfter(
-                        new AuthenticationLoggingFilter(),
-                        BasicAuthenticationFilter.class)
+        http.addFilterAt(staticKeyAuthenticationFilter, BasicAuthenticationFilter.class)
                 .authorizeRequests().anyRequest().permitAll();
 
         return http.build();
     }
-
 }
