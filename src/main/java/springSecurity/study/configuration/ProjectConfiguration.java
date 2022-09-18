2a38df123705da7676dbcec31d3a9aa57223ed28
Nuouung <jin6016@gmail.com>
2022-09-18 11:53:57 +0900

    CSRF 토큰에 대해 이해하기

--- src/main/java/springSecurity/study/configuration/ProjectConfiguration.java
+++ src/main/java/springSecurity/study/configuration/ProjectConfiguration.java
@@ -1,32 +1,23 @@
 package springSecurity.study.configuration;
 
-import lombok.RequiredArgsConstructor;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
-import springSecurity.study.security.filter.AuthenticationLoggingFilter;
-import springSecurity.study.security.filter.RequestValidationFilter;
-import springSecurity.study.security.filter.StaticKeyAuthenticationFilter;
-
-import javax.servlet.Filter;
+import org.springframework.security.web.csrf.CsrfFilter;
+import springSecurity.study.security.filter.CsrfTokenLogger;
 
 @Configuration
-@EnableWebSecurity
-@RequiredArgsConstructor
 public class ProjectConfiguration {
-
-    private final StaticKeyAuthenticationFilter staticKeyAuthenticationFilter;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
-        http.addFilterAt(staticKeyAuthenticationFilter, BasicAuthenticationFilter.class)
-                .authorizeRequests().anyRequest().permitAll();
+        http.addFilterAfter(new CsrfTokenLogger(), CsrfFilter.class)
+                .authorizeRequests()
+                .anyRequest().permitAll();
 
         return http.build();
+
     }
 }
--- src/main/java/springSecurity/study/controller/HelloController.java
+++ src/main/java/springSecurity/study/controller/HelloController.java
@@ -1,22 +1,19 @@
 package springSecurity.study.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import javax.servlet.http.HttpServletRequest;
 
 @RestController
 public class HelloController {
 
     @GetMapping("/hello")
-    public String hello(HttpServletRequest request) {
+    public String getHello() {
+        return "GET /hello";
+    }
 
-        String requestId = request.getHeader("Request-Id");
-        String loggingResult = (Boolean) request.getAttribute("loggingResult") ?
-                "인증 로그 기록 완료" : "인증 로그 기록 실패";
-
-        return requestId + " (" + loggingResult + ")";
+    @PostMapping("/hello")
+    public String postHello() {
+        return "POST /hello";
     }
 }
