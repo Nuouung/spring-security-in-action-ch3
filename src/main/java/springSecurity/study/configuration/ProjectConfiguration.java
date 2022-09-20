f1c31a3df61552b66da285480982aa56ea0f10dc
Nuouung <jin6016@gmail.com>
2022-09-20 09:18:29 +0900

    웹에서 csrf 토근 사용하기

--- src/main/java/springSecurity/study/configuration/ProjectConfiguration.java
+++ src/main/java/springSecurity/study/configuration/ProjectConfiguration.java
@@ -3,19 +3,48 @@ package springSecurity.study.configuration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.csrf.CsrfFilter;
 import springSecurity.study.security.filter.CsrfTokenLogger;
 
 @Configuration
+@EnableWebSecurity
 public class ProjectConfiguration {
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        InMemoryUserDetailsManager inMemoryUserDetailsManager = new InMemoryUserDetailsManager();
+
+        UserDetails user = User.withUsername("jinseok")
+                .password("1234")
+                .authorities("READ")
+                .build();
+
+        inMemoryUserDetailsManager.createUser(user);
+
+        return inMemoryUserDetailsManager;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
-        http.addFilterAfter(new CsrfTokenLogger(), CsrfFilter.class)
-                .authorizeRequests()
-                .anyRequest().permitAll();
+        http.authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .defaultSuccessUrl("/main", true);
 
         return http.build();
 
