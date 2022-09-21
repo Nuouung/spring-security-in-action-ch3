d9a94d3088a20bca22b9f5121497dd48b06f4b0e
Nuouung <jin6016@gmail.com>
2022-09-21 20:05:41 +0900

    cors 실습 준비

--- src/main/java/springSecurity/study/configuration/ProjectConfiguration.java
+++ src/main/java/springSecurity/study/configuration/ProjectConfiguration.java
@@ -1,28 +1,20 @@
 package springSecurity.study.configuration;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
-import springSecurity.study.security.csrf.repository.CustomCsrfTokenRepository;
 
 @Configuration
 @EnableWebSecurity
-@RequiredArgsConstructor
 public class ProjectConfiguration {
 
-    private final CustomCsrfTokenRepository customCsrfTokenRepository;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
-        http.csrf(httpSecurityCsrfConfigurer -> {
-            httpSecurityCsrfConfigurer
-                    .csrfTokenRepository(customCsrfTokenRepository);
-            httpSecurityCsrfConfigurer.ignoringAntMatchers("/ciao"); // /ciao 경로는 csrf 필터 적용을 무시하라
-        });
+        http.csrf().disable();
 
         http.authorizeRequests().anyRequest().permitAll();
 
--- src/main/java/springSecurity/study/controller/HelloController.java
+++ src/main/java/springSecurity/study/controller/HelloController.java
@@ -1,24 +1,24 @@
 package springSecurity.study.controller;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-@RestController
+@Controller
+@Slf4j
 public class HelloController {
 
-    @GetMapping("/hello")
+    @GetMapping("/")
     public String getHello() {
-        return "GET /hello";
+        return "main";
     }
 
-    @PostMapping("/hello")
-    public String postHello() {
-        return "POST /hello";
-    }
-
-    @PostMapping("/ciao")
-    public String postCiao() {
-        return "POST /ciao";
+    @PostMapping("/test")
+    @ResponseBody
+    public String test() {
+        log.info("테스트 메소드 호출");
+        return "테스트 성공";
     }
 }
